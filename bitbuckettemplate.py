9b1b084cb9097b9b1fa68e720c840f327ba8e0cd
Ray (Cobra) Tran <rayctran@gmail.com>
2024-08-09 05:03:40 -0500

    Update bitbuckettemplate.py
    Added CSV export

--- bitbuckettemplate.py
+++ bitbuckettemplate.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -33,7 +34,7 @@ def get_repository_permissions(repo_slug):
     return response.json()['values']
 
 def create_structure():
-    structure = {}
+    structure = []
 
     projects = get_projects()
 
@@ -41,11 +42,8 @@ def create_structure():
         project_key = project['key']
         project_name = project['name']
         
-        # Create project entry with permissions
-        structure[project_name] = {
-            'permissions': get_project_permissions(project_key),
-            'repositories': {}
-        }
+        # Get project permissions
+        project_permissions = get_project_permissions(project_key)
 
         repositories = get_repositories(project_key)
 
@@ -53,14 +51,46 @@ def create_structure():
             repo_name = repo['name']
             repo_slug = repo['slug']
             
-            # Add repository entry with permissions
-            structure[project_name]['repositories'][repo_name] = {
-                'details': repo,
-                'permissions': get_repository_permissions(repo_slug)
-            }
+            # Get repository permissions
+            repo_permissions = get_repository_permissions(repo_slug)
+            
+            # Add project and repository details to structure
+            structure.append({
+                'project_name': project_name,
+                'repo_name': repo_name,
+                'project_permissions': project_permissions,
+                'repo_permissions': repo_permissions
+            })
 
     return structure
 
+def write_to_csv(data, filename='bitbucket_structure.csv'):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        # Write the header
+        writer.writerow(['Project Name', 'Repository Name', 'User/Group', 'Permission Level', 'Scope'])
+        
+        # Write the data
+        for item in data:
+            project_name = item['project_name']
+            repo_name = item['repo_name']
+
+            # Write project permissions
+            for perm in item['project_permissions']:
+                user_or_group = perm['user']['display_name'] if 'user' in perm else perm['group']['name']
+                permission_level = perm['permission']
+                scope = 'Project'
+                writer.writerow([project_name, repo_name, user_or_group, permission_level, scope])
+
+            # Write repository permissions
+            for perm in item['repo_permissions']:
+                user_or_group = perm['user']['display_name'] if 'user' in perm else perm['group']['name']
+                permission_level = perm['permission']
+                scope = 'Repository'
+                writer.writerow([project_name, repo_name, user_or_group, permission_level, scope])
+
 if __name__ == '__main__':
     bitbucket_structure = create_structure()
-    print(bitbucket_structure)
+    write_to_csv(bitbucket_structure)
+    print("CSV file created successfully.")
